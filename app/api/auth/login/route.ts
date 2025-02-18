579ee32013aa216ffdefd0490c28c195eec79bae
eunwoo-levi <eunwoo1341@gmail.com>
2025-02-19 02:47:56 +0900

    feat: 로그인 성공시 refreshToken을 Cookie에 담음

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -30,10 +30,19 @@ export async function POST(req: NextRequest) {
 
     // refreshToken을 쿠키에 저장
     const cookieStore = await cookies();
+
+    cookieStore.set('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 60 * 30, // 30분 (accessToken 만료 시간)
+    });
+
     cookieStore.set('refreshToken', refreshToken, {
       httpOnly: true, // 클라이언트에서 접근 불가
       secure: process.env.NODE_ENV === 'production', // 프로덕션에서는 HTTPS 사용
-      sameSite: 'strict', // 크로스사이트 요청 방지
+      sameSite: 'strict', // (CSRF)크로스사이트 요청 방지
       path: '/', // 쿠키를 모든 경로에서 사용 가능
       maxAge: 60 * 60 * 24 * 7, // 7일 동안 유지
     });
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
         <div className='text-md mt-4 flex justify-center text-gray-600'>
           <p className='mr-2'>You don't have an Account?</p>
           <Link href='/register' className='font-semibold text-blue-500 hover:underline'>
-            login
+            register
           </Link>
         </div>
       </div>
