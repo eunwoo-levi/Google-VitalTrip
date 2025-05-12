b369f60c79f51f25c5c5f1c0f71fc3c5fd21afa7
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-13 01:22:23 +0900

    feat: Googlemap SearchBar 구현

--- src/features/googleMap/ui/GoogleMaps.tsx
+++ src/features/googleMap/ui/GoogleMaps.tsx
@@ -4,26 +4,27 @@ import React, { useRef, useState } from 'react';
 import { initializeMap } from '../utils/initializeMap';
 import { findNearbyPlaces } from '../utils/findNearbyPlaces';
 import { goToCurrentLocation } from '../utils/goToCurrentLocation';
+import SearchBar from './SearchBar';
 
 export default function GoogleMaps() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [mapInstance, setMapInstance] = useState<google.maps.Map | null>(null);
   const [service, setService] = useState<google.maps.places.PlacesService | null>(null);
 
-  // 지도 초기화
   initializeMap({ mapRef, setMapInstance, setService, findNearbyPlaces });
-
-  // 현위치로 이동
 
   return (
     <div className='absolute inset-0'>
-      <div ref={mapRef} className='h-full w-full' />
+      <SearchBar service={service} mapInstance={mapInstance} />
+
       <button
         onClick={() => goToCurrentLocation({ mapInstance, service })}
         className='absolute top-[240px] right-[6px] z-10 rounded-full bg-white p-3 shadow-lg transition hover:bg-gray-100'
       >
         📍
       </button>
+
+      <div ref={mapRef} className='h-full w-full' />
     </div>
   );
 }
