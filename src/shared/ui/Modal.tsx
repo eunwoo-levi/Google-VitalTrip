092959a1c645e7a616cb537ef0eb914f61dfd1a3
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-12 20:46:26 +0900

    feat: createPortal 사용하여 컴포넌트의 렌더링 위치를 DOM 계층에서 분리

--- src/shared/ui/Modal.tsx
+++ src/shared/ui/Modal.tsx
@@ -2,6 +2,7 @@
 
 import React, { useRef } from 'react';
 import { useOutsideClick } from '../hooks/useOutsideClick';
+import { createPortal } from 'react-dom';
 
 interface ModalProps {
   onClose: () => void;
@@ -12,11 +13,13 @@ export default function Modal({ onClose, children }: ModalProps) {
   const modalRef = useRef<HTMLDivElement>(null) as React.RefObject<HTMLDivElement>;
   useOutsideClick(modalRef, onClose);
 
-  return (
+  const modalRoot = typeof window !== 'undefined' ? document.body : null;
+
+  const modalContent = (
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50'>
       <div
         ref={modalRef}
-        className='animate-slideFadeUp relative w-[95%] max-w-md rounded-xl bg-white p-6 shadow-xl'
+        className='animate-slideFadeUp relative w-[95%] rounded-xl bg-white p-6 shadow-xl'
       >
         <button
           onClick={onClose}
@@ -28,4 +31,6 @@ export default function Modal({ onClose, children }: ModalProps) {
       </div>
     </div>
   );
+
+  return modalRoot ? createPortal(modalContent, modalRoot) : null;
 }
