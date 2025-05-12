035e082f163f1c3831ea715cc96972fbbfc03433
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-12 14:15:42 +0900

    refactor: useSymptomStore 에서 persist 적용하여 새로고침해도 유지되도록 구현

--- src/features/firstAid/store/useSymptomStore.ts
+++ src/features/firstAid/store/useSymptomStore.ts
@@ -1,15 +1,23 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface SymptomState {
   symptomType: string;
   symptomDetail: string;
-  setSymptomData: (symptomType: string, symptomDetail: string) => void;
+  setSymptomData: (type: string, detail: string) => void;
   resetSymptom: () => void;
 }
 
-export const useSymptomStore = create<SymptomState>((set) => ({
-  symptomType: '',
-  symptomDetail: '',
-  setSymptomData: (symptomType, symptomDetail) => set({ symptomType, symptomDetail }),
-  resetSymptom: () => set({ symptomType: '', symptomDetail: '' }),
-}));
+export const useSymptomStore = create<SymptomState>()(
+  persist(
+    (set) => ({
+      symptomType: '',
+      symptomDetail: '',
+      setSymptomData: (symptomType, symptomDetail) => set({ symptomType, symptomDetail }),
+      resetSymptom: () => set({ symptomType: '', symptomDetail: '' }),
+    }),
+    {
+      name: 'symptom-storage',
+    },
+  ),
+);
