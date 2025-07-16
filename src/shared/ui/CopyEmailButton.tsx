d77dac047dffaf707ae8f145a725fa80062547dd
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-17 00:29:15 +0900

    refactor CopyEmailButton 컴포넌트 비즈니스 로직 hook으로 분리

--- src/shared/ui/CopyEmailButton.tsx
+++ src/shared/ui/CopyEmailButton.tsx
@@ -4,9 +4,9 @@ import { useState } from 'react';
 import { MdContactSupport } from 'react-icons/md';
 import { FaCheck } from 'react-icons/fa';
 
-export default function CopyEmailButton() {
+
+const useCopyEmail = (email: string) => {
   const [copied, setCopied] = useState(false);
-  const email = 'eunwoo1341@gmail.com';
 
   const handleCopy = async () => {
     try {
@@ -14,14 +14,22 @@ export default function CopyEmailButton() {
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     } catch (err) {
-      // 보안상 상세한 에러 정보는 로깅하지 않음
       console.error('이메일 복사 실패');
     }
-  };
+  }
+
+  return { copied, onCopy: handleCopy };
+}
+
+export default function CopyEmailButton() {
+  const email = 'eunwoo1341@gmail.com';
+
+  const { onCopy, copied } = useCopyEmail(email);
+
 
   return (
     <button
-      onClick={handleCopy}
+      onClick={onCopy}
       className='flex items-center gap-2 rounded-full bg-blue-50 px-6 py-2 text-lg font-bold text-blue-700 shadow transition hover:bg-blue-100'
     >
       {copied ? (
