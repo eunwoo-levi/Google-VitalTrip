8c23d67f1ace12602c398b801061a1bafc2731a2
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-01 06:42:34 +0900

    chore: 불필요한 코드 제거

--- src/features/auth/types/signup.ts
+++ src/features/auth/types/signup.ts
@@ -1,18 +1,3 @@
-export interface SignupRequest {
-  email: string;
-  name: string;
-  password: string;
-  passwordConfirm: string;
-  birthDate: string;
-  countryCode: string;
-  phoneNumber: string;
-}
-
-export interface SignupResponse {
-  message: string;
-  data: SignupFormData;
-}
-
 export interface SignupFormData {
   email: string;
   password: string;
