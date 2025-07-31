07eb99e57fab5817763ee3fb0f56947dac8e1805
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-31 21:08:12 +0900

    refactor: logout 함수 POST로 변경

--- src/features/auth/api/logoutUser.ts
+++ src/features/auth/api/logoutUser.ts
@@ -1,6 +1,11 @@
 export const logoutUser = async () => {
   try {
-    const response = await fetch('/api/auth/logout');
+    const response = await fetch('/api/auth/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     if (!response.ok) {
       throw new Error('로그아웃 실패');
     }
