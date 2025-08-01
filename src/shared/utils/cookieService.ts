33ac90a38f5b9cfe8f210d14817776961411d25a
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-02 02:53:46 +0900

    feat: refresh 로직 추가하여 getValidAccessToken 유틸 함수 구현

--- src/shared/utils/cookieService.ts
+++ src/shared/utils/cookieService.ts
@@ -1,13 +1,41 @@
 import { cookies } from 'next/headers';
+import { httpServer } from './httpServer';
 
-export const getAccessToken = async () => {
+interface ValidAccessTokenResponse {
+  message: string;
+  data: {
+    accessToken: string;
+  };
+}
+
+export async function getValidAccessToken(): Promise<string | null> {
   const cookieStore = await cookies();
-  const accessToken = cookieStore.get('accessToken');
-  if (!accessToken) {
-    return null;
+  let accessToken = cookieStore.get('accessToken')?.value;
+  const refreshToken = cookieStore.get('refreshToken')?.value;
+
+  if (!accessToken && refreshToken) {
+    try {
+      const response: ValidAccessTokenResponse = await httpServer.post('/auth/refresh', {
+        refreshToken,
+      });
+
+      accessToken = response.data.accessToken;
+      cookieStore.set('accessToken', accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        path: '/',
+        maxAge: 60 * 60,
+      });
+
+      return accessToken;
+    } catch (error) {
+      return null;
+    }
   }
-  return accessToken?.value;
-};
+
+  return accessToken ?? null;
+}
 
 export const getTempToken = async () => {
   const cookieStore = await cookies();
