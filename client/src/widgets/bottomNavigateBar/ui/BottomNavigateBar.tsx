a1c86bfe3e11e367b87604d9de3044de83319868
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-05 16:13:15 +0900

    refactor: 역할에 맞게 UI로직 분리

--- client/src/widgets/bottomNavigateBar/ui/BottomNavigateBar.tsx
+++ client/src/widgets/bottomNavigateBar/ui/BottomNavigateBar.tsx
@@ -2,6 +2,7 @@
 
 import Chatbot from '@/src/features/chatbot/ui/Chatbot';
 import { useSymptomStore } from '@/src/features/firstAid/store/useSymptomStore';
+import { ProfileModal } from '@/src/features/profile/ui/ProfileModal';
 import { useOutsideClick } from '@/src/shared/hooks/useOutsideClick';
 import Dropdown from '@/src/shared/ui/Dropdown';
 import Modal from '@/src/shared/ui/Modal';
@@ -59,81 +60,34 @@ export default function BottomNavigateBar() {
         <Link href='/translate' className={linkClassName}>
           <MdGTranslate size={25} />
         </Link>
-        <button onClick={() => setIsProfileModalOpen((prev) => !prev)} className={linkClassName}>
+        <button onClick={() => setIsProfileModalOpen(true)} className={linkClassName}>
           <Image src='/logo.webp' alt='logo' width={25} height={25} className='object-contain' />
         </button>
+        {isProfileModalOpen && (
+          <Modal key='profile-modal' onClose={() => setIsProfileModalOpen(false)}>
+            <ProfileModal />
+          </Modal>
+        )}
         <button onClick={() => setIsSymptomModalOpen((prev) => !prev)} className={linkClassName}>
           <FaRegHospital size={25} />
         </button>
-        <div className='relative'>
-          <button onClick={() => setIsMenuOpen((prev) => !prev)} className={linkClassName}>
-            <TiThMenu size={25} />
-          </button>
-          {isMenuOpen && (
-            <Dropdown ref={menuRef} direction='top'>
-              <ul className='p-2'>
-                {MENU_ITEMS.map((item) => (
-                  <li key={item.code} className='flex flex-col items-center'>
-                    {item.code === 'ABOUT_US' ? (
-                      <Link
-                        href='/about'
-                        className='mb-2 hover:cursor-pointer hover:text-blue-500'
-                        onClick={() => setIsMenuOpen(false)}
-                      >
-                        {item.label}
-                      </Link>
-                    ) : (
-                      <button
-                        onClick={() => setInfoModalCode(item.code)}
-                        className='mb-2 hover:cursor-pointer hover:text-blue-500'
-                      >
-                        {item.label}
-                      </button>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </Dropdown>
-          )}
-        </div>
+        <MenuButton
+          isMenuOpen={isMenuOpen}
+          menuRef={menuRef}
+          setIsMenuOpen={setIsMenuOpen}
+          setInfoModalCode={setInfoModalCode}
+        />
         <Chatbot />
       </div>
 
       {isSymptomModalOpen && (
-        <Modal key='symptom-modal' onClose={closeSymptomModal}>
-          <h2 className='mb-4 text-center text-xl font-bold'>Describe your symptom</h2>
-          <select
-            className='mb-4 w-full rounded-md border border-gray-300 p-2 text-sm font-semibold focus:ring-2 focus:ring-blue-400 focus:outline-none'
-            onChange={(e) => updateSymptomData(e)}
-            value={symptomData.type}
-          >
-            <option value='' disabled>
-              Select symptom
-            </option>
-            {SYMPTOMS.map((symptom) => (
-              <option key={symptom.code} value={symptom.code} className='font-semibold'>
-                {symptom.label}
-              </option>
-            ))}
-          </select>
-          <textarea
-            className='mb-4 h-32 w-full resize-none rounded-md border border-gray-300 p-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none'
-            placeholder='Describe your symptoms in detail'
-            value={symptomData.detail}
-            onChange={(e) => updateSymptomData(e)}
-          />
-          <button
-            disabled={isDisabled}
-            onClick={handleSubmit}
-            className={`w-full rounded-md px-4 py-2 text-white transition-colors duration-200 ${
-              isDisabled
-                ? 'cursor-not-allowed bg-gray-300'
-                : 'cursor-pointer bg-blue-500 hover:bg-blue-600'
-            }`}
-          >
-            Submit
-          </button>
-        </Modal>
+        <SymptomModal
+          closeSymptomModal={closeSymptomModal}
+          updateSymptomData={updateSymptomData}
+          symptomData={symptomData}
+          isDisabled={isDisabled}
+          handleSubmit={handleSubmit}
+        />
       )}
       {infoModalCode === 'EMERGENCY' && (
         <Modal key='emergency-modal' onClose={() => setInfoModalCode(null)}>
@@ -148,3 +102,98 @@ export default function BottomNavigateBar() {
     </>
   );
 }
+
+const MenuButton = ({
+  isMenuOpen,
+  menuRef,
+  setIsMenuOpen,
+  setInfoModalCode,
+}: {
+  isMenuOpen: boolean;
+  menuRef: React.Ref<HTMLDivElement>;
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setInfoModalCode: React.Dispatch<React.SetStateAction<string | null>>;
+}) => (
+  <div className='relative'>
+    <button onClick={() => setIsMenuOpen((prev) => !prev)} className={linkClassName}>
+      <TiThMenu size={25} />
+    </button>
+    {isMenuOpen && (
+      <Dropdown ref={menuRef} direction='top'>
+        <ul className='p-2'>
+          {MENU_ITEMS.map((item) => (
+            <li key={item.code} className='flex flex-col items-center'>
+              {item.code === 'ABOUT_US' ? (
+                <Link
+                  href='/about'
+                  className='mb-2 hover:cursor-pointer hover:text-blue-500'
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {item.label}
+                </Link>
+              ) : (
+                <button
+                  onClick={() => setInfoModalCode(item.code)}
+                  className='mb-2 hover:cursor-pointer hover:text-blue-500'
+                >
+                  {item.label}
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </Dropdown>
+    )}
+  </div>
+);
+
+const SymptomModal = ({
+  closeSymptomModal,
+  updateSymptomData,
+  symptomData,
+  isDisabled = false,
+  handleSubmit,
+}: {
+  closeSymptomModal: () => void;
+  updateSymptomData: (e: React.ChangeEvent<HTMLSelectElement | HTMLTextAreaElement>) => void;
+  symptomData: { type: string; detail: string };
+  isDisabled: boolean;
+  handleSubmit: () => void;
+}) => {
+  return (
+    <Modal key='symptom-modal' onClose={closeSymptomModal}>
+      <h2 className='mb-4 text-center text-xl font-bold'>Describe your symptom</h2>
+      <select
+        className='mb-4 w-full rounded-md border border-gray-300 p-2 text-sm font-semibold focus:ring-2 focus:ring-blue-400 focus:outline-none'
+        onChange={(e) => updateSymptomData(e)}
+        value={symptomData.type}
+      >
+        <option value='' disabled>
+          Select symptom
+        </option>
+        {SYMPTOMS.map((symptom) => (
+          <option key={symptom.code} value={symptom.code} className='font-semibold'>
+            {symptom.label}
+          </option>
+        ))}
+      </select>
+      <textarea
+        className='mb-4 h-32 w-full resize-none rounded-md border border-gray-300 p-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none'
+        placeholder='Describe your symptoms in detail'
+        value={symptomData.detail}
+        onChange={(e) => updateSymptomData(e)}
+      />
+      <button
+        disabled={isDisabled}
+        onClick={handleSubmit}
+        className={`w-full rounded-md px-4 py-2 text-white transition-colors duration-200 ${
+          isDisabled
+            ? 'cursor-not-allowed bg-gray-300'
+            : 'cursor-pointer bg-blue-500 hover:bg-blue-600'
+        }`}
+      >
+        Submit
+      </button>
+    </Modal>
+  );
+};
