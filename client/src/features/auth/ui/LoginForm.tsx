309dc209f13225e635b86ce3d691adb51efa0227
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-07 02:59:25 +0900

    feat: useLogin 훅 적용

--- client/src/features/auth/ui/LoginForm.tsx
+++ client/src/features/auth/ui/LoginForm.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { APIError } from '@/src/shared/utils/apiError';
-import { useState } from 'react';
-import { useLoginMutation } from '../api/useLoginMutation';
+import { useLogin } from '../hooks/useLogin';
 
 interface FormData {
   email: string;
@@ -10,41 +8,19 @@ interface FormData {
 }
 
 export default function LoginForm() {
-  const [formData, setFormData] = useState<FormData>({
-    email: '',
-    password: '',
-  });
-  const [error, setError] = useState<string | null>(null);
-  const { mutateAsync: loginUser, isPending, error: loginError } = useLoginMutation();
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-    if (error) setError(null);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError(null);
-
-    try {
-      await loginUser(formData);
-    } catch (error) {
-      if (error instanceof APIError) {
-        setError(error.message);
-      } else {
-        setError(loginError?.message || 'Login failed. Please try again.');
-      }
-    }
-  };
-
-  const isFormValid = formData.email && formData.password;
+  const {
+    loginForm,
+    updateLoginForm,
+    handleLoginSubmit,
+    isPending,
+    isValid,
+    invalidErrors,
+    isError,
+    error,
+  } = useLogin();
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col'>
+    <form onSubmit={handleLoginSubmit} className='flex flex-col'>
       <div className='mb-4 flex flex-col'>
         <label htmlFor='email' className='text-md mb-2 font-semibold text-gray-600'>
           Email
@@ -53,13 +29,14 @@ export default function LoginForm() {
           id='email'
           name='email'
           type='email'
-          value={formData.email}
-          onChange={handleChange}
+          value={loginForm.email}
+          onChange={(e) => updateLoginForm('email', e.target.value)}
           placeholder='example@example.com'
           className='w-full rounded-md border border-gray-300 p-3 outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-400 disabled:bg-gray-100'
           disabled={isPending}
           required
         />
+        {invalidErrors.email && <p className='text-sm text-red-600'>{invalidErrors.email}</p>}
       </div>
 
       <div className='mb-6 flex flex-col'>
@@ -70,25 +47,22 @@ export default function LoginForm() {
           id='password'
           name='password'
           type='password'
-          value={formData.password}
-          onChange={handleChange}
+          value={loginForm.password}
+          onChange={(e) => updateLoginForm('password', e.target.value)}
           placeholder='Enter your password'
           className='w-full rounded-md border border-gray-300 p-3 outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-400 disabled:bg-gray-100'
           disabled={isPending}
           required
         />
+        {invalidErrors.password && <p className='text-sm text-red-600'>{invalidErrors.password}</p>}
       </div>
 
-      {error && (
-        <div className='mb-4 rounded-md border border-red-200 bg-red-50 p-3'>
-          <p className='text-sm text-red-600'>{error}</p>
-        </div>
-      )}
+      {isError && <p className='text-sm text-red-600'>{error?.message}</p>}
 
       <button
         type='submit'
         className='w-full rounded-md bg-blue-500 py-3 font-semibold text-white transition-colors duration-300 hover:bg-blue-600 disabled:cursor-not-allowed disabled:bg-blue-300'
-        disabled={isPending || !isFormValid}
+        disabled={isPending || !isValid}
       >
         {isPending ? (
           <div className='flex items-center justify-center'>
