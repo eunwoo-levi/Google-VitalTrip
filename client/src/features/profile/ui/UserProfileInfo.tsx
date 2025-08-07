e353a00331a5dc3cb6a48822c4834ea6efc07491
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-08 01:00:19 +0900

    styles: profile loading layout style 변경

--- client/src/features/profile/ui/UserProfileInfo.tsx
+++ client/src/features/profile/ui/UserProfileInfo.tsx
@@ -153,7 +153,7 @@ const ProfilePhoneNumber = ({
 
 const ProfileLoading = () => {
   return (
-    <div className='flex min-h-[400px] items-center justify-center'>
+    <div className='mx-auto flex h-[400px] w-[320px] items-center justify-center md:w-[600px]'>
       <div className='flex flex-col items-center space-y-4'>
         <div className='h-8 w-8 animate-spin rounded-full border-4 border-blue-500 border-t-transparent'></div>
         <p className='text-gray-600'>Loading profile...</p>
