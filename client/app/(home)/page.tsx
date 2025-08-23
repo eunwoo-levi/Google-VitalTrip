0094bcac85df79c12dc5ac98e77212cf54e0eef7
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-23 22:57:29 +0900

    feat: MapPage에 MedicalList보여주는 SidePanel 추가

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import MedicalList from '@/src/features/medical/ui/MedicalList';
+import { usePreventKeyboardZoom } from '@/src/shared/hooks/usePreventKeyboardZoom';
+import SidePanel from '@/src/shared/ui/SidePanel';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
 
 const GoogleMaps = dynamic(() => import('@/src/features/googleMap/ui/GoogleMaps'), { ssr: false });
 
 export default function MapPage() {
+  usePreventKeyboardZoom();
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
+
+  const toggleSidePanel = () => {
+    setIsSidePanelOpen(!isSidePanelOpen);
+  };
+
   return (
-    <div className='mx-auto min-h-screen w-full'>
+    <div className='relative mx-auto min-h-screen w-full'>
+      <SidePanel isOpen={isSidePanelOpen} onToggle={toggleSidePanel}>
+        <MedicalList />
+      </SidePanel>
       <GoogleMaps />
     </div>
   );
