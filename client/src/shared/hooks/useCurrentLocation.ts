482824670fb1298660af1977093e4eedcd993b4d
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-23 23:28:23 +0900

    feat: 임시 coordsMock 추가

--- client/src/shared/hooks/useCurrentLocation.ts
+++ client/src/shared/hooks/useCurrentLocation.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 // 나중에 제거하기!
 const coordsMock = {
@@ -7,36 +7,36 @@ const coordsMock = {
 };
 
 export const useCurrentLocation = () => {
-  const [coords, setCoords] = useState<{ latitude: number; longitude: number } | null>(null);
+  // const [coords, setCoords] = useState<{ latitude: number; longitude: number } | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (!navigator.geolocation) {
-      setError('Geolocation is not supported by this browser.');
-      setIsLoading(false);
-      return;
-    }
+  // useEffect(() => {
+  //   if (!navigator.geolocation) {
+  //     setError('Geolocation is not supported by this browser.');
+  //     setIsLoading(false);
+  //     return;
+  //   }
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setCoords({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (err) => {
-        setError(err.message);
-        setIsLoading(false);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 0,
-      },
-    );
-  }, []);
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       setCoords({
+  //         latitude: position.coords.latitude,
+  //         longitude: position.coords.longitude,
+  //       });
+  //       setIsLoading(false);
+  //     },
+  //     (err) => {
+  //       setError(err.message);
+  //       setIsLoading(false);
+  //     },
+  //     {
+  //       enableHighAccuracy: true,
+  //       timeout: 10000,
+  //       maximumAge: 0,
+  //     },
+  //   );
+  // }, []);
 
   return { coords: coordsMock, isLoading, error };
 };
