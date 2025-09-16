c7ae7b1979a609fefad897a04982bc5370d7521b
eunwoo-levi <eunwoo1341@gmail.com>
2025-09-17 00:00:28 +0900

    feat: 메인 페이지에서 GoogleMaps은 바로 호출되도록 수정

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,19 +1,10 @@
 'use client';
 
+import GoogleMaps from '@/src/features/googleMap/ui/GoogleMaps';
 import MedicalList from '@/src/features/medical/ui/MedicalList';
 import { usePreventKeyboardZoom } from '@/src/shared/hooks/usePreventKeyboardZoom';
 import SidePanel from '@/src/shared/ui/SidePanel';
-import dynamic from 'next/dynamic';
 import { useState } from 'react';
-
-const GoogleMaps = dynamic(() => import('@/src/features/googleMap/ui/GoogleMaps'), {
-  ssr: false,
-  loading: () => (
-    <div className='flex h-full w-full items-center justify-center bg-gray-100'>
-      <div className='text-gray-600'>Loading...</div>
-    </div>
-  ),
-});
 
 export default function MapPage() {
   usePreventKeyboardZoom();
