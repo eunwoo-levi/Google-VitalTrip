4b9ec5f997952f0901dd9fe32f311ff3143650c5
eunwoo-levi <eunwoo1341@gmail.com>
2025-09-24 09:54:29 +0900

    feat: admin 페이지 route 설정

---
+++ admin/src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
+import DashboardPage from './pages/DashboardPage';
+import Layout from './pages/Layout';
+import LoginPage from './pages/LoginPage';
+import { ProtectedRoute } from './pages/ProtectedRoute';
+
+const ROUTES = {
+  ROOT: '/',
+  LOGIN: '/login',
+};
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<Layout />}>
+          <Route path={ROUTES.ROOT} element={<DashboardPage />} />
+        </Route>
+      </Route>
+    </>
+  )
+);
