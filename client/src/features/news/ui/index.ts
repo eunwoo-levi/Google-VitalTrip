df3b5c981b27744cd66f048d13d3ec20ac791ed4
eunwoo-levi <eunwoo1341@gmail.com>
2026-05-15 15:53:02 +0900

    refactor: 뉴스 페이지 제거 및 응급처치 백과사전 페이지로 교체

--- client/src/features/news/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { LoadingSkeleton } from './LoadingSkeleton';
-export { NewsCard } from './NewsCard';
-export { NewsFooter } from './NewsFooter';
-export { NewsGrid } from './NewsGrid';
-export { NewsHeader } from './NewsHeader';
-export { NewsPageClient } from './NewsPageClient';
-export { Pagination } from './Pagination';
-export { RevalidateButton } from './RevalidateButton';
